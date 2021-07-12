0d1ff24e02ba4bcd33671944a61a5f257041aafd
Fabrício Brazil <81660822+Braz99@users.noreply.github.com>
2021-07-12 19:50:37 -0300

    cria o context para o carrinho

---
+++ src/provider/cart.js
@@ -0,0 +1,15 @@
+import React, { useState, useContext } from "react";
+
+export const CartContext = React.createContext();
+
+export const CartProvider = (props) => {
+	let cart = useState([]);
+
+	return (
+		<CartContext.Provider value={{ cart }}>
+			{props.children}
+		</CartContext.Provider>
+	);
+};
+
+export const useCart = () => useContext(CartContext);
