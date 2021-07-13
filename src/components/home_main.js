6812714ea1259934e6abf34fb76e4fc06f0b2ddc
Fabrício Brazil <81660822+Braz99@users.noreply.github.com>
2021-07-13 14:42:12 -0300

    Ajusta a lógica de compra

--- src/components/home_main.js
+++ src/components/home_main.js
@@ -14,13 +14,18 @@ export default function HomeMain() {
 
 		function handleBuy(name, price, image) {
 			if (cart.find((objeto) => name === objeto.name)) {
+				
 				let item = cart.find((objeto) => (name === objeto.name ? objeto : ""));
+			
 
 				item.qtd += 1;
+				
+				item.price += item.price / (item.qtd - 1);
 
-				item.price *= item.qtd;
+
 			} else {
-				cart.push({ name: name, price: price, qtd: 1, image: image });
+				cart.push({ 'name': name, 'price': price, 'qtd': 1, 'image': image })
+				
 			}
 		}
 
