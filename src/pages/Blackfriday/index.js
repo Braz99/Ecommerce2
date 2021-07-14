64a5af517d4768420915a2ec6f2a47bfd1bb12e7
Fabrício Brazil <81660822+Braz99@users.noreply.github.com>
2021-07-14 15:57:11 -0300

    Cria e adiciona estilo para a pagina Blackfriday

--- src/pages/Blackfriday/index.js
+++ src/pages/Blackfriday/index.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import "./blackfriday_s.css";
+import { useHistory } from "react-router";
 
-export default function Blackfriday(){
-    return(
-        <></>
-    )
+export default function Blackfriday() {
+	let history = useHistory();
+	return (
+		<main id="container_friday">
+			<h1 id="title_promo">
+				<span id="title_marine">Marine</span> Friday = Promoções surpreendentes!
+			</h1>
+
+			<p id="text">
+				Móveis e eletrodomésticos com até 50% de desconto. A{" "}
+				<span id="title_marine">Marine</span> Friday está chegando, não perca
+				nenhuma <span id="promotion">promoção</span>, cadastre já o teu email!
+			</p>
+
+			<form id="form_email">
+				<input type="email" name="email" placeholder="Email"></input>
+				<button id="email_button">Cadastrar</button>
+			</form>
+
+			<button
+				id="home_button"
+				onClick={() => {
+					history.push("./");
+				}}>
+				Ir para o site
+			</button>
+		</main>
+	);
 }
